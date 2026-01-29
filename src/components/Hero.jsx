58c5ef5698aaa5c1bd04d4ae9abb6b21e5df2c8d
fadjar <fadjar.iftikhar@gmail.com>
2026-01-29 21:02:56 +0700

    edit component hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
 
 const Hero = () => {
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", { type: "chars, words" });
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSplit.chars.forEach((char) => {
+      char.classList.add("text-gradient");
+    });
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      stagger: 0.06,
+      ease: "expo.out",
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      stagger: 0.06,
+      ease: "expo.out",
+      delay: 1,
+    });
+  }, []);
+
   return (
     <>
       <section id="hero" className="noisy">
@@ -11,6 +38,31 @@ const Hero = () => {
           alt="left leaf"
           className="left-leaf"
         />
+
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right leaf"
+          className="right-leaf"
+        />
+
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic</p>
+              <p className="subtitle">
+                Sip the Spirit <br /> Summer
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktails on our menu are made with the finest ingredients
+                and crafted by our expert bartenders.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
